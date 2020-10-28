5c46894cdafa7c2935fa80479548c46267a9b1d4
ddiyoo <ddiyoo@gmail.com>
2020-10-28 20:47:35 +0900

    Modified main.py
    add discriminator function

--- main.py
+++ main.py
@@ -2,37 +2,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import layers
+# def generator():
+#
+#     model = tf.keras.Sequential()
+#     ## 25*25*128: 시퀀스 오브젝트 모델에 25*25*128개의 노드를 Dense 레이어를 통해 연결(?), bias 안씀, input 데이터의 shape이 (100,0)
+#     model.add(layers.Dense(25 * 25 * 128, use_bias=False,input_shape=(100,)))
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.ReLU())
+#
+#     model.add(layers.Reshape((25, 25, 128)))
+#     ## output_shape 이 25, 25, 128이 아닐경우 assertion error 출력
+#     assert model.output_shape == (None, 25, 25, 128)
+#
+#     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+#     assert model.output_shape == (None, 25, 25, 128)
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.ReLU())
+#
+#     model.add(layers.Conv2DTranspose(64, (5,5), strides=(2,2), padding='same', use_bias=False))
+#     assert model.output_shape == (None, 50, 50, 64)
+#     model.add(layers.BatchNormalization())
+#     model.add(layers.ReLU())
+#
+#     ## 최종적으로 50*50*64의 이미지가 나오도록 업샘플링링
+#
+#    return model
+
 def generator():
-    ##
     model = tf.keras.Sequential()
-    ## 25*25*128: 시퀀스 오브젝트 모델에 25*25*128개의 노드를 Dense 레이어를 통해 연결(?), bias 안씀, input 데이터의 shape이 (100,0)
-    model.add(layers.Dense(25 * 25 * 128, use_bias=False,input_shape=(100,)))
-
-    ## BatchNorm 추가
+    model.add(layers.Dense(25*25*128, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
-    # print(model.variables)
-    ## activation function : relu
     model.add(layers.ReLU())
-    print(model.variables)
-    # Reshape을 왜해주지??
-    model.add(layers.Reshape((25, 25, 128)))
-    # print(model.variables)
-    assert model.output_shape == (None, 25, 25, 128)
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 25,25,128)
+    ## filter의 수, kernel(filter)의 size, padding = 'same'-> output_size를 input_size와 똑같이 함
+    model.add(layers.Conv2DTranspose(128, (5,5), strides=(1,1), padding='same', use_bias=False))
     assert model.output_shape == (None, 25, 25, 128)
     model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5,5), strides=(2,2), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(64, (5,5),strides=(2,2),padding='same', use_bias=False ))
     assert model.output_shape == (None, 50, 50, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
-
-    return model
-
 
 g_test = generator()
 # test = np.random.randint(0,225, size=100)
 test = tf.random.normal([1,100])
 g_test(test)
+
+def discriminator():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5,5), strides=(2,2), padding='same', input_shape=[100,100,3]))
+    model.add(layers.ReLU(0.2))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5,5), strides=(2,2), padding='same'))
+    model.add(layers.ReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+    model.summary()
+    return model
